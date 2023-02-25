45cea8d08efeefccff4f9b94016af6bf3d220778
Freebudd <elyboka99@gmail.com>
2023-02-25 10:52:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints "programming is like building a multilingual
- * puzzle, followed by a newline."
- * return 0.
+ * puzzle, followed by newline."
+ * return: This returns 0.
  */
 
 int main(void)
